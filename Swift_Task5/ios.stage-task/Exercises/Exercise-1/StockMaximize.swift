a4561c390ae3ca1aaa306d79da1956d1536fb7a4
rolll13 <r.mikulich@mail.ru>
2021-06-13 03:06:00 +0300

    feat:add exercise1 implementtation

--- Swift_Task5/ios.stage-task/Exercises/Exercise-1/StockMaximize.swift
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-1/StockMaximize.swift
@@ -3,6 +3,28 @@ import Foundation
 class StockMaximize {
 
     func countProfit(prices: [Int]) -> Int {
-        return -1
+        var profit = 0
+        if prices.isEmpty || prices.sorted(by: >)==prices.sorted(by: <){ //проверка на пустой массив и на заполенность одинаковыми элементами
+            return profit
+        }
+        var prices = prices
+        var newPrices: [Int] = []
+
+        if prices.min()==prices.last && (prices.removeLast() != 0) && prices.sorted(by: >)==prices.sorted(by: <){ //проверка копии массива что можно купить по цене ниже до поднятия
+            return profit
+        }
+        
+        for i in (prices.firstIndex(of: prices.min()!)!)..<prices.count{ 
+            newPrices.append(prices[i])
+        }
+        
+        while (newPrices.count>1){
+            for _ in 0...newPrices.firstIndex(of: newPrices.max()!)!{
+                profit+=(newPrices.max()!-newPrices[0])
+                newPrices.remove(at: 0)
+                }
+        }
+        return profit
     }
 }
+//минимальный элемент -> к максимальному, минимальный не последний, максимальный после минимального
