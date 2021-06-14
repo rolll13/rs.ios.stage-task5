56a25b8f8eaf28be04d5aa731dc54055107b21ee
rolll13 <r.mikulich@mail.ru>
2021-06-14 23:45:18 +0300

    feat: implementation part of 2nd task

--- Swift_Task5/ios.stage-task/Exercises/Exercise-2/MessageDecryptor.swift
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-2/MessageDecryptor.swift
@@ -3,6 +3,31 @@ import UIKit
 class MessageDecryptor: NSObject {
     
     func decryptMessage(_ message: String) -> String {
-        return ""
+        var textOfMessage = message
+        var number = 0
+        var text = ""
+        var resultText = ""
+        var textNumber = ""
+        
+        for i in textOfMessage{
+            if i.isNumber{
+                textNumber.append(i)
+                textOfMessage.removeFirst()
+            } else if i != "[" && i != "]" {
+                text.append(i)
+            }
+            
+        }
+        if textNumber.isEmpty{
+            resultText = text
+        } else {
+            number = Int(textNumber)!
+            while number>0{
+                resultText+=text
+                number-=1
+            }
+        }
+        
+        return resultText
     }
 }
